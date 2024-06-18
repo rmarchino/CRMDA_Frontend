6bae22491ddc65f97105526e5b9ed4cbf90896a6
Robert Steyker <140686632+rmarchino@users.noreply.github.com>
2024-06-18 00:19:25 -0500

    Update axios.js
    modificando url

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "https://da-backend-6fei.onrender.com"
 });
 
 export default clienteAxios;
